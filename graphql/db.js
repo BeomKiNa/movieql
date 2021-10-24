53d7631195994589a59d1ca6272d28f7d991b9e4
beomki69 <beomki69@gmail.com>
2021-10-25 00:52:41 +0900

    Wrapping a REST API with GraphQL

---
+++ graphql/db.js
@@ -0,0 +1,17 @@
+import fetch from "node-fetch";
+
+const API_URL = "https://yts.mx/api/v2/list_movies.json?";
+
+export const getMovies = async (limit, rating) => {
+  let REQUEST_URL = API_URL;
+  if (limit > 0) {
+    REQUEST_URL += `limit=${limit}`;
+  }
+  if (rating > 0) {
+    REQUEST_URL += `&minimum_rating=${rating}`;
+  }
+  const {
+    data: { movies },
+  } = await (await fetch(REQUEST_URL)).json();
+  return movies;
+};
